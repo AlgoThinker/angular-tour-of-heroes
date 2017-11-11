b4d0a71fa2edcba41a4b3f165eeda744fa52339e
Tushar Sharma <tushar123cse@gmail.com>
2017-11-11 18:06:07 +0530

    second commit includes changes for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
-  {path : 'heroes', component : HeroesComponent}
+  {path : '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path : 'heroes', component : HeroesComponent},
+  {path : 'dashboard', component : DashboardComponent},
+  {path : 'detail/:id', component : HeroDetailComponent}
 ];
 
 @NgModule({
@@ -16,5 +19,3 @@ const routes: Routes = [
 export class AppRoutingModule {
 
 }
-
-
